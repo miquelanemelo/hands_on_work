adfeb8c2872a2fd67bccbd7f8986358e3902c349
Miquelane Melo <miquelanemiquelanemelo@gmail.com>
2020-04-04 21:03:08 -0300

    Calculadora decimal para binario

--- 1-semestre/c++/calculadora/Calculadora.cpp
+++ 1-semestre/c++/calculadora/Calculadora.cpp
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>  
 
 using namespace std;  
 
 int main()  {  
 
-int a[10], n, i;    
-	cout<<"Enter the number to convert: ";    
-cin>>n; 
+// declaração das variaveis
+int a[10], numeroDecimal, i;  
+  
+cout<<"Digite um numero a ser convertido: ";
+
+//Recebe o valor digitado
+cin>>numeroDecimal; 
    
-for(i=0; n>0; i++) {    
-	a[i]=n%2;    
-	n= n/2;  
-}    
-
-
-
-cout<<"Binary of the given number= ";    
-
+for(i=0; numeroDecimal>0; i++) {    
+	a[i]=numeroDecimal%2;    
+	numeroDecimal= numeroDecimal/2;  
+}   
+cout<<"O numero em binario eh: ";
+    
+//Informa o numero em binario
 for(i=i-1 ;i>=0 ;i--) {    
 cout<<a[i];    
 }    
